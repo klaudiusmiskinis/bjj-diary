a6ee21f71d4712dcc5f3d04e3caf688f0ee5e186
Klaudius Miskinis <klaudiusmiskinis@gmail.com>
2022-11-01 22:18:50 +0100

    Eliminando funcionamiento

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,34 +1,18 @@
+/* GLOBAL VARS */
 var dailies;
+delete dailies;
+
+function previousDate() {
+  console.log("previous");
+}
+
+function nextDate() {
+  console.log("next");
+}
 
 function init() {
   dailies = new InteractDaily(dailies);
   dailies.splitDates();
-  const menuMain = document.querySelector("#menu-main-head");
-  menuMain.insertAdjacentHTML("afterbegin", menuYears(dailies));
-  console.log(menuYears(dailies));
-}
-
-function menuYears(dailies) {
-  let render = "";
-  dailies.dailies.forEach((date) => {
-    console.log(date);
-    render = `"<li class="mb-1">
-  <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed text-light"
-    data-bs-toggle="collapse"
-    data-bs-target="#${replaceSlashWithHyphen(date.id)}"
-    aria-expanded="false">
-    ${date.year}
-  </button>
-    <div class="collapse" id="${replaceSlashWithHyphen(date.id)}">
-      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-      ${dailies.getMonthElements(dailies.dailies)}
-      </ul>
-    </div>
-  </li>"`;
-  });
-  return render;
-    dailies = new InteractDaily(dailies);
-    dailies.splitDates();
 }
 
 const req = new XMLHttpRequest();
